89fee2c95aeb852b451ac6cce947fc10f047192c
luczett <40020857+luczett@users.noreply.github.com>
2021-05-15 16:46:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,13 +5,16 @@ class NextcloudCalendar(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler('calendar.nextcloud.intent')
+    @intent_file_handler('calendar.nextcloud.intent')
     def handle_calendar_nextcloud(self, message):
-        day = message.data.get('day')
-        self.speak_dialog('calendar.nextcloud', data=day)
+        try:
+            day = message.data.get("day")
+            self.speak_dialog("calendar.nextcloud", data=day)
         
-        self.log.info("I am a DEBUG INformation")
-
+            self.log.info("I am a DEBUG INformation")
+            pass
+        except:
+            self.speak_dialog("schedule_error")
 
 def create_skill():
     return NextcloudCalendar()
